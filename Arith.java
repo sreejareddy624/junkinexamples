31f79ef34413df289be39c6d76f220dad3af967c
sreejareddy624 <245122749010@mvsrec.edu.in>
2025-03-25 15:40:32 +0530

    Add files via upload

---
+++ Arith.java
@@ -0,0 +1,21 @@
+public class Arith {
+    public static void main(String[] args) {
+        double r1 = 10.5;  // First number
+        double r2 = 5.5;   // Second number
+
+        // Perform arithmetic operations
+        double sum = r1 + r2;
+        double difference = r1 - r2;
+        double product = r1 * r2;
+        double quotient = r1 / r2;
+
+        // Display results
+        System.out.println("First Number: " + r1);
+        System.out.println("Second Number: " + r2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
+
